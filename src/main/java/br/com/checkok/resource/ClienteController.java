f0a06dc42616d5bc711a7d7b09a928109b1313fd
Carlos Aruh <carlos.aruh@gmail.com>
2024-09-20 12:48:57 -0300

    PUT for idempotence

--- src/main/java/br/com/checkok/resource/ClienteController.java
+++ src/main/java/br/com/checkok/resource/ClienteController.java
@@ -14,8 +14,8 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
-import jakarta.ws.rs.PATCH;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
@@ -60,7 +60,7 @@ public class ClienteController {
         		.build();
     }
 
-    @PATCH
+    @PUT
     @Path("/{id}")
     public Response atualizarClienteParcial(@PathParam Long id, Cliente cliente) {
         Optional<Cliente> clienteExistente = clienteDao.findById(id);
